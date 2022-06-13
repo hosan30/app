10b9baa8fa29e7b7ae1ab7012da6c5ae1aecfaa5
hosan30 <50867996+hosan30@users.noreply.github.com>
2022-06-13 16:57:40 +0600

    sacbas

--- beta-script.js
+++ beta-script.js
@@ -1,25 +1,90 @@
 ///////////test script
 
-view();
-navigator.sayswho = (function() {
-    var ua = navigator.userAgent;
-    var tem;
-    var M =
-        ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-    if (/trident/i.test(M[1])) {
-        tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
-        return 'IE ' + (tem[1] || '');
-    }
-    if (M[1] === 'Chrome') {
-        tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
-        if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
-    }
-    M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
-    if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
-    return M.join(' ');
-})();
+// view();
+alert(navigator.appName);
 
-alert(navigator.sayswho);
+// navigator.sayswho = (function() {
+//     var ua = navigator.userAgent;
+//     var tem;
+//     var M =
+//         ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+//     if (/trident/i.test(M[1])) {
+//         tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+//         return 'IE ' + (tem[1] || '');
+//     }
+//     if (M[1] === 'Chrome') {
+//         tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+//         if (tem != null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+//     }
+//     M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+//     if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+//     return M.join(' ');
+// })();
+
+// alert(navigator.sayswho);
+// function get_browser() {
+//     var ua = navigator.userAgent,
+//         tem,
+//         M =
+//         ua.match(
+//             /(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i
+//         ) || [];
+//     if (/trident/i.test(M[1])) {
+//         tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+//         return { name: 'IE', version: tem[1] || '' };
+//     }
+//     if (M[1] === 'Chrome') {
+//         tem = ua.match(/\bOPR|Edge\/(\d+)/);
+//         if (tem != null) {
+//             return { name: 'Opera', version: tem[1] };
+//         }
+//     }
+//     M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+//     if ((tem = ua.match(/version\/(\d+)/i)) != null) {
+//         M.splice(1, 1, tem[1]);
+//     }
+//     return {
+//         name: M[0],
+//         version: M[1],
+//     };
+// }
+
+// var browser = get_browser(); // browser.name = 'Chrome'
+// // browser.version = '40'
+
+// console.log(browser);
+
+// navigator.browserSpecs = (function() {
+//     var ua = navigator.userAgent,
+//         tem,
+//         M =
+//         ua.match(
+//             /(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i
+//         ) || [];
+//     if (/trident/i.test(M[1])) {
+//         tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+//         return { name: 'IE', version: tem[1] || '' };
+//     }
+//     if (M[1] === 'Chrome') {
+//         tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+//         if (tem != null)
+//             return { name: tem[1].replace('OPR', 'Opera'), version: tem[2] };
+//     }
+//     M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+//     if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+//     return { name: M[0], version: M[1] };
+// })();
+
+// console.log(navigator.browserSpecs); //Object { name: "Firefox", version: "42" }
+
+// if (navigator.browserSpecs.name == 'Firefox') {
+//     // Do something for Firefox.
+//     if (navigator.browserSpecs.version > 42) {
+//         // Do something for Firefox versions greater than 42.
+//     }
+// } else {
+//     // Do something for all other browsers.
+// }
 
 // function savedata(data) {
 //     fetch('https://jsonplaceholder.typicode.com/posts', {
